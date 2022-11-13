b46b462a3fd3cedbe55a34bb5885b6522e6b9061
Maximiliano <max.borquezc@gmail.com>
2022-11-13 04:01:56 -0300

    Inicializacion de proyecto

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-//Hola mundo, soy Max
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log("El proyecto esta corriendo en el puerto => ", process.env.PORT);
+});
+
+//const productRoutes = require("./routes/productRoutes");
+
+app.use(cors());
+app.use(express.json());
+app.options("*", cors());
+//app.use("/api", productRoutes);
+
+mongoose.set("useFindAndModify", false);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+mongoose.connect(process.env.DB, (err) => {
+    if (err) {
+        return console.log("Error al conectar con la base de datos", err);
+    }
+    return console.log("Conectado a la base de datos");
+});
