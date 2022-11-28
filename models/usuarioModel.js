6b25a21d51ea845420f57214ebec8518a7efcc09
kmiloconk <kmilo.alejan@gmail.com>
2022-11-27 22:24:33 -0300

    usuario

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const usiaroSchema = new Schema({
+const usuarioSchema = new Schema({
     rut: {
         type: String,
         required: true
